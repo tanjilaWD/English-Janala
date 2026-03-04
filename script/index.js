53fcae54d5861a2e447ec58ef5d7ce36b721d4eb
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-04 18:53:42 +0600

    feat: active button add

--- script/index.js
+++ script/index.js
@@ -3,12 +3,23 @@ const loadLessons =()=>{
     .then(res => res.json()) //promise of json data
     .then((json) => displayLesson(json.data));
 };
+
+const removeActive=()=>{
+    const lessonButtons = document.querySelectorAll('.lesson-btn');
+    // console.log(lessonButtons);
+    lessonButtons.forEach(btn => btn.classList.remove('active'));
+}
 const loadLevelWord =(id)=>{
     const url = `https://openapi.programming-hero.com/api/level/${id}` ;
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayloadLevelWord(data.data));
-}
+    .then((data)=>{
+        removeActive(); //remove all active class
+        const clickBtn = document.getElementById(`lesson-btn-${id}`);
+        clickBtn.classList.add("active"); // add active class
+        displayloadLevelWord(data.data)
+    });
+};
 const displayloadLevelWord =(words)=>{
 const wordContainer = document.getElementById('word-container');
 wordContainer.innerHTML = '';
@@ -53,7 +64,7 @@ const displayLesson=(lessons)=>{
    // 3. create element
    const btnDiv = document.createElement('div');
    btnDiv.innerHTML = `
-     <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
+     <button id="lesson-btn-${lesson.level_no}" onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button>
    
    `;
 
