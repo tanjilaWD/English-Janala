7af77d9f29298ae8241624dc21bca40d90ac1fa6
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-03-05 22:58:25 +0600

    feat: speak

--- script/index.js
+++ script/index.js
@@ -3,6 +3,12 @@ const createElements = (arr) =>{
     return htmlElements.join('');
 };
 
+function pronounceWord(word) {
+  const utterance = new SpeechSynthesisUtterance(word);
+  utterance.lang = "en-EN"; // English
+  window.speechSynthesis.speak(utterance);
+}
+
 const manageSpinner =(status)=>{
     if(status == true){
         document.getElementById('spinner').classList.remove('hidden');
@@ -97,7 +103,7 @@ words.forEach(word => {
 
         <button onclick="loadWordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
 
-        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+        <button onclick="pronounceWord('${word.word}')" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
     `
